5cd83f365080a219fd33df97e4e743d94448ca06
Alpaslan Akgün <alpaslan.akgun@outlook.com>
2024-11-05 23:37:06 +0300

    usermanager updated

--- EduConnect.Service/Concrete/UserManager.cs
+++ EduConnect.Service/Concrete/UserManager.cs
@@ -6,57 +6,50 @@ using EduConnect.Core.Results.ComplexType;
 using EduConnect.Services.Abstract;
 using EduConnect.Services.Common;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace EduConnect.Services.Concrete
 {
-    public class UserManager: IUserService
+    public class UserManagerService : IUserService
     {
-        private readonly UserManager<User> _userManager;
+        private readonly UserManager<User> _identityUserManager; 
         private readonly IMapper _mapper;
-        public UserManager(UserManager<User> userManager,IMapper mapper)
+
+        public UserManagerService(UserManager<User> userManager, IMapper mapper)
         {
-            _userManager = userManager;
+            _identityUserManager = userManager;
             _mapper = mapper;
         }
+
         public async Task<IDataResult<UserDto>> CreateUserAsync(CreateUserDto createUserDto)
         {
-        
             var user = new User
             {
                 Email = createUserDto.Email,
                 UserName = createUserDto.UserName
             };
 
-       
-            var result = await _userManager.CreateAsync(user, createUserDto.Password);
+            var result = await _identityUserManager.CreateAsync(user, createUserDto.Password);
 
             if (!result.Succeeded)
             {
                 var errors = result.Errors.Select(x => x.Description).ToList();
-                return new ErrorDataResult<UserDto>(string.Join(", ", errors)); 
+                return new ErrorDataResult<UserDto>(string.Join(", ", errors));
             }
 
             var userDto = _mapper.Map<UserDto>(user);
-
-            return new SuccessDataResult<UserDto>(userDto, UserAuthenticationMessageConstant.UserSuccessful); 
+            return new SuccessDataResult<UserDto>(userDto, UserAuthenticationMessageConstant.UserSuccessful);
         }
-
 
         public async Task<IDataResult<UserDto>> GetUserByNameAsync(string userName)
         {
-            var user = await _userManager.FindByNameAsync(userName);
+            var user = await _identityUserManager.FindByNameAsync(userName);
             if (user == null)
             {
-                return new ErrorDataResult<UserDto>(UserAuthenticationMessageConstant.UserIdNotFound); 
+                return new ErrorDataResult<UserDto>(UserAuthenticationMessageConstant.UserIdNotFound);
             }
             var userDto = _mapper.Map<UserDto>(user);
             return new SuccessDataResult<UserDto>(userDto);
         }
-
     }
 }
